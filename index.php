dc5f76e29dce4d4acdc53d0893f2f84ad27281ff
Charuka454 <charukawimalarathne@gmail.com>
2025-05-25 02:38:10 -0700

    version 1.0

--- index.php
+++ index.php
@@ -4,13 +4,17 @@ include("config.php");
 
 if( isset($_POST["submit"])){
 
+    //for product
+    $pName = $_POST['Pname'];
+    $price = $_POST['Pprice'];
+
     $fileName = $_FILES["product"]["name"];
     $fileTmpName = $_FILES["product"]["tmp_name"];
     $folder = 'img/'.$fileName;
-    $query ="INSERT INTO product (file) VALUES ('$fileName')";
+    $query ="INSERT INTO products (name,price,file) VALUES ('$pName','$price','$fileName')";
    $quuu = mysqli_query($con,$query);
    if(move_uploaded_file($fileTmpName,$folder)){
-    echo"<h2>file uplode</h2>";
+    echo"file uplode";
    }else{
      echo"<h2>file not uplode</h2>";
    }
@@ -19,28 +23,58 @@ if( isset($_POST["submit"])){
 ?>
 <!DOCTYPE html>
 <html lang="en">
+    <style>
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+div {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+</style>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
+    <div>
     <form method="POST" enctype="multipart/form-data">
+        <label for="Pname">Product Name</label>
+        <input type="text" id="Pname" name="Pname" placeholder="Product name..">
+        <label for="Pprice">Product Price</label>
+        <input type="text" id="Pprice" name="Pprice" placeholder="Product price..">
+        <label for="Ppicture">Product Picture</label><br>
         <input type="file" name="product">
-        <button type="submit" name="submit" >add</button>
+        <input type="submit" value="Submit" name="submit">
 
     </form>
-    <div>
-        <?php
-        $rresult = mysqli_query($con,"SELECT * FROM product");
-
-        while($row = mysqli_fetch_assoc($rresult)){
-?>
-<img src="img/<?php echo $row['file'];?>"> 
-<?php
-        }
-        
-        ?>
     </div>
+    
+  
+   
 </body>
 </html>
